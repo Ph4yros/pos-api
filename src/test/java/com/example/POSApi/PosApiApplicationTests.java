c939877b7bc683cc960ef6dfa63445ecd0c618ba
Eren <eren.lgl.3@gmail.com>
2025-08-12 13:45:30 +0300

    Heroku commit

---
+++ src/test/java/com/example/POSApi/PosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.POSApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
